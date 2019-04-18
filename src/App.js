7654cb7624dcfee3fa825335c2088d4cd71a3e8c
Achmad Kurnianto <achmad.kurnianto@gmail.com>
2019-04-18 08:44:15 +0700

    load iframe after form submit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    dashboardBlobUrl: null
+  }
+  async componentDidMount () {
+    document.getElementById('dashboard-hidden-form').submit()
+    // const response = await fetch('http://localhost:3300/dashboard', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-Type': 'application/json'
+    //   },
+    //   body: JSON.stringify({
+    //     type: 'hello'
+    //   }),
+    //   mode: 'cors'
+    // });
+    // const dashboardTemplate = await response.text()
+    // const blob = new Blob([dashboardTemplate], { type: "text/html" })
+    // const dashboardBlobUrl = URL.createObjectURL(blob)
+    // this.setState({
+    //   dashboardBlobUrl
+    // })
+  }
   render() {
+    // const { dashboardBlobUrl } = this.state
+    // return (
+    //   <iframe title='dashboard-page' src={dashboardBlobUrl} />
+    // )
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <>
+        <form id='dashboard-hidden-form' encType='application/json' target='dashboard-page' method='POST' action='http://localhost:3300/dashboard'>
+          <input type='hidden' name='type' value='pr-vs-po' />
+        </form>
+        <iframe title='dashboard-page' name='dashboard-page' style={{ width: '100%', height: '99vh', border: 0 }} />
+      </>
     );
   }
 }
